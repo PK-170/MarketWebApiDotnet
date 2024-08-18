137dc9dd26c0d7628e6985f7528bc7f7eb009896
pawan kumar <pawan59c@gmail.com>
2024-08-18 21:39:05 +1000

    removed some file

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7541ab16da09c1208f57ea3422d97710273a9137")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01ef4f0d5019b9bf265eadf2a7a21124ec143770")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
