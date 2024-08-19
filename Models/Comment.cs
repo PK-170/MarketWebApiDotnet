fadde46cffa5be8e3964a4aa92ec4a60adf02fb8
pawan kumar <pawan59c@gmail.com>
2024-08-19 22:13:36 +1000

    added dependency

--- Models/Comment.cs
+++ Models/Comment.cs
@@ -10,7 +10,8 @@ namespace api.Models
         public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Content { get; set; } = string.Empty;
+        public int? StockId { get; set; }
+        public Stock? Stock { get; set; }
 
-        
     }
 }
