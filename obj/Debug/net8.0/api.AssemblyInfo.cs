47e54aea966fc7e0cfa6bf0fe8d5d8998c870b1e
pawan kumar <pawan59c@gmail.com>
2024-08-21 20:33:36 +1000

    added get method

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08ee8100f3a359182c9914f6b4440ea6638477ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6110bd196512c87b900446adcba5384cec634070")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
