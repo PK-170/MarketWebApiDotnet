826b164be44a68b07b94fcf54f713850bca3e7ce
pawan kumar <pawan59c@gmail.com>
2024-08-26 17:50:40 +1000

    update method done

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08c17655321b0ad8ed339eded0c842ece39c4115")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c180faef619c7a1e8a81fab423b548d2e8ad5891")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
