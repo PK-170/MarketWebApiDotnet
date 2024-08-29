a035a0221503808c4e57fa629a805587903b1b39
pawan kumar <pawan59c@gmail.com>
2024-08-29 21:47:00 +1000

    get all method works

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7aa02f16bc111bb94c44a21ca9221e3b52a99ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d4697ace5d75c6eafa60869dd0569f9ce229251")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
