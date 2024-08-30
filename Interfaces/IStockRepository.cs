c759edb89656f1b6f1f74cb4022ce3167a0b2936
pawan kumar <pawan59c@gmail.com>
2024-08-30 13:07:32 +1000

    Interface method

--- Interfaces/IStockRepository.cs
+++ Interfaces/IStockRepository.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Dtos.Stock;
 using api.Models;
 
 namespace api.Interfaces
@@ -11,6 +12,8 @@ namespace api.Interfaces
         Task<List<Stock>> GetAllAsync();
         Task<Stock> GetByIdAsync(int id);
         Task<Stock> CreateAsync(Stock stockModel);
+        Task<Stock?> UpdateAsync(int id, UpdateStockRequestDto stockDto);
+        Task<Stock?> DeleteAsync(int id);
         
     }
 }
