feec3572004a2e9a3c1e56f601daaa9ac8915f4f
pawan kumar <pawan59c@gmail.com>
2024-08-31 15:24:34 +1000

    repo async

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d4697ace5d75c6eafa60869dd0569f9ce229251")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c8d93792993258621c59ff227a83d1c6cb14f04")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
