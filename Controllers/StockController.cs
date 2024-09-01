58eb968eaf5100ab2bdaa6b241ce31d53300c1d6
pawan kumar <pawan59c@gmail.com>
2024-09-01 18:48:55 +1000

    gtbyid methods

--- Controllers/StockController.cs
+++ Controllers/StockController.cs
@@ -36,7 +36,7 @@ namespace api.Controllers
         
         public  async Task<IActionResult> GetById([FromRoute] int id)
         {
-          var stock = await _context.Stocks.FindAsync(id);
+          var stock = await _stockRepo.GetByIdAsync(id);
           if(stock == null)
           {
             return NotFound();
