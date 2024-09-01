b75a2d911c6566c3bc20ebecc16edf946692e620
pawan kumar <pawan59c@gmail.com>
2024-09-01 18:54:23 +1000

    update  methods

--- Controllers/StockController.cs
+++ Controllers/StockController.cs
@@ -63,7 +63,6 @@ namespace api.Controllers
                           return NotFound();
                      }
 
-                      await _context.SaveChangesAsync();
                       return Ok(stockModel.ToStockDto());
         }
 
