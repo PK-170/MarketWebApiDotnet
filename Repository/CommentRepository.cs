fd4140934664f6881c0a74cff2fa60fa0a32bc8b
pawan kumar <pawan59c@gmail.com>
2024-09-02 20:07:37 +1000

    comment repository

--- Repository/CommentRepository.cs
+++ Repository/CommentRepository.cs
@@ -10,8 +10,10 @@ namespace api.Repository
 {
     public class CommentRepository : ICommentRepository
     {
+        private readonly ApplicationDBContext _context;
         public CommentRepository(ApplicationDBContext context)
         {
+            _context = context;
             
         }
         public Task<List<Comment>> GetAllAsync()
