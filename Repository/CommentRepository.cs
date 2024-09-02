ab801e7ab3e3dc3865db319875450f3d088c1252
pawan kumar <pawan59c@gmail.com>
2024-09-02 20:05:16 +1000

    comment repository

--- Repository/CommentRepository.cs
+++ Repository/CommentRepository.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Data;
 using api.Interfaces;
 using api.Models;
 
@@ -9,6 +10,10 @@ namespace api.Repository
 {
     public class CommentRepository : ICommentRepository
     {
+        public CommentRepository(ApplicationDBContext context)
+        {
+            
+        }
         public Task<List<Comment>> GetAllAsync()
         {
             throw new NotImplementedException();
