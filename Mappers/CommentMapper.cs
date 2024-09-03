adcdd4a23a453ac554d10f64e72737a56a4e1e6b
pawan kumar <pawan59c@gmail.com>
2024-09-03 18:13:32 +1000

    comment controller getall method

---
+++ Mappers/CommentMapper.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Dtos.Comment;
+using api.Models;
+
+namespace api.Mappers
+{
+    public class CommentMapper
+    {
+        public static CommentDto ToCommentDto(this Comment commentModel){
+
+        }
+    }
+}
