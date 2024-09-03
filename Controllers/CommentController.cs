4505956e6bca6c7312af982db4dcc036ca0b3c6e
pawan kumar <pawan59c@gmail.com>
2024-09-03 17:57:20 +1000

    comment controller

--- Controllers/CommentController.cs
+++ Controllers/CommentController.cs
@@ -2,11 +2,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers
 {
-    public class CommentController
+    [Route("api/comment")]
+    [ApiController]
+    public class CommentController : ControllerBase
     {
+        private readonly ICommentRepository _commentRepo;
+        public CommentController(ICommentRepository commentRepo)
+        {
+            _commentRepo = commentRepo;
+        }
         
     }
 }
