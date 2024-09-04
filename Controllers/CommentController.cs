0d7f5d6c9970c68379d78c166938cfea811c46b8
pawan kumar <pawan59c@gmail.com>
2024-09-04 19:35:02 +1000

    comment controller getall method

--- Controllers/CommentController.cs
+++ Controllers/CommentController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Interfaces;
+using api.Mappers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers
@@ -20,7 +21,10 @@ namespace api.Controllers
         [HttpGet]
         public async Task<IActionResult> GetAll(){
             
-            var comments = await 
+            var comments = await _commentRepo.GetAllAsync();
+            var commentDto = comments.Select(s => s.ToCommentDto());
+
+            return Ok(commentDto);
         }
         
     }
