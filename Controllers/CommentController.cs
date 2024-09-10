41a657ab62d8f918cab94c0cbff7cc1b195a5102
pawan kumar <pawan59c@gmail.com>
2024-09-10 20:55:45 +1000

    added create async method

--- Controllers/CommentController.cs
+++ Controllers/CommentController.cs
@@ -50,7 +50,8 @@ namespace api.Controllers
                      }
 
                      var commentModel = commentDto.ToCommentFromCreate(stockId);
-                     await _commentRepo.CreateAsync();
+                     await _commentRepo.CreateAsync(commentModel);
+                     return CreatedAtAction(nameof(GetByIdAsync), new {id = commentModel}, commentModel.ToCommentDto());
                   
         }
 
