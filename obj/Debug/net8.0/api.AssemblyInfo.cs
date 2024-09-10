c91b09f8dc04288c53570bc246e7839739769916
pawan kumar <pawan59c@gmail.com>
2024-09-10 21:31:03 +1000

    create method is working

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4be6e1defe8b771996cbeac4463d32656e41aa7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+016566eca0d0da29924d1d8267691aec268f9f51")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
