4e359da54bb801df3307ecbbbb35b1d91581fff1
pawan kumar <pawan59c@gmail.com>
2024-09-11 20:00:10 +1000

    update method async

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+819c269aa86c9d464468a15a7eb440d88ee53902")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6c15eb5717d097876c0bd2a24e04de003a13605")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
