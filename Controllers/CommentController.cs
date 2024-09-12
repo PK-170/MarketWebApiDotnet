08b81f88ab043cbdfcee14bf408d10145429abab
pawan kumar <pawan59c@gmail.com>
2024-09-12 22:48:36 +1000

    added update dto

--- Controllers/CommentController.cs
+++ Controllers/CommentController.cs
@@ -59,7 +59,7 @@ namespace api.Controllers
         [Route ("{id}")]
         public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateCommentRequestDto updateDto){
  
-                      var comment = await _commentRepo.UpdateAsync(id);
+                      var comment = await _commentRepo.UpdateAsync(id, );
 
         }
 
