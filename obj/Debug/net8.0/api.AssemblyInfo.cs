db139b45ab454d25f9f6d5a28ff3a1b6f2c76c90
pawan kumar <pawan59c@gmail.com>
2024-09-14 11:58:00 +1000

    fixed comment delete method

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3183cc042939c5038a053946335361c62a16dad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b104954680d0c1518ec4d2571363de0249d580d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
