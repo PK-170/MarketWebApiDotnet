b5110f519bac003470bc101e9fb12dae4b9e0a16
pawan kumar <pawan59c@gmail.com>
2024-09-15 19:06:36 +1000

    data validation

--- Dtos/Stock/CreateStockRequestDto.cs
+++ Dtos/Stock/CreateStockRequestDto.cs
@@ -12,6 +12,9 @@ namespace api.Dtos.Stock
         [MaxLength(10, ErrorMessage ="the symbol canot be more than 10 character")]
         public string Symbol { get; set; } = string.Empty;
 
+        [Required]
+        [MaxLength(10, ErrorMessage ="the CompanyName canot be more than 10 character")]
+
         public string CompanyName { get; set; } = string.Empty;
 
         public decimal Purchase { get; set; }
