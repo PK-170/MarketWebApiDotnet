932dba30bf7ee11d5aa59fb5ae915aeabde3dd86
pawan kumar <pawan59c@gmail.com>
2024-09-15 18:54:41 +1000

    data validation

--- Dtos/Comment/CreateCommentDto.cs
+++ Dtos/Comment/CreateCommentDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,8 @@ namespace api.Dtos.Comment
 {
     public class CreateCommentDto
     {
+        [Required]
+        [MinLength(5,ErrorMessage = "Title must be 5 characters")]
           public string Title { get; set; } = string.Empty;
           public string Content { get; set; } = string.Empty;
 
