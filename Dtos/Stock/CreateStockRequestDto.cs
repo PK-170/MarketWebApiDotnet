2710582a3a1d1b5e442cc8f752685c9cd8006c95
pawan kumar <pawan59c@gmail.com>
2024-09-16 20:11:55 +1000

    data validation

--- Dtos/Stock/CreateStockRequestDto.cs
+++ Dtos/Stock/CreateStockRequestDto.cs
@@ -17,6 +17,8 @@ namespace api.Dtos.Stock
 
         public string CompanyName { get; set; } = string.Empty;
 
+        [Required]
+        [Range(1,1000000000)]
         public decimal Purchase { get; set; }
         public decimal LastDiv { get; set; }
 
