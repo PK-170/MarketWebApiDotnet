7298212c7f3f070ca32e9b983398cb60e943f827
pawan kumar <pawan59c@gmail.com>
2024-09-16 20:18:02 +1000

    data validation

--- Dtos/Stock/UpdateStockRequestDto.cs
+++ Dtos/Stock/UpdateStockRequestDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,12 @@ namespace api.Dtos.Stock
 {
     public class UpdateStockRequestDto
     {
+        [Required]
+        [MaxLength(10, ErrorMessage ="the symbol canot be more than 10 character")]
         public string Symbol { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(10, ErrorMessage ="the CompanyName canot be more than 10 character")]
 
         public string CompanyName { get; set; } = string.Empty;
 
