1978a4408994f7a2a15335c93e21e5daa206f7f0
pawan kumar <pawan59c@gmail.com>
2024-09-18 18:48:20 +1000

    filter options

--- Controllers/StockController.cs
+++ Controllers/StockController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
 using api.Dtos.Stock;
+using api.Helpers;
 using api.Interfaces;
 using api.Mappers;
 using Microsoft.AspNetCore.Mvc;
@@ -25,7 +26,7 @@ namespace api.Controllers
 
         [HttpGet]
 
-        public async Task<IActionResult> GetAll()
+        public async Task<IActionResult> GetAll([FromQuery] QueryObjects query)
         {
             if(!ModelState.IsValid){
                 return BadRequest(ModelState);
