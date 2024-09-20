ab3749682da9368662c78208e27b57426b113bc5
pawan kumar <pawan59c@gmail.com>
2024-09-20 21:54:55 +1000

    page size

--- Helpers/QueryObject.cs
+++ Helpers/QueryObject.cs
@@ -15,5 +15,6 @@ namespace api.Helpers
          public bool IsDecsending { get; set; } = false;
 
          public int PageNumber { get; set; } = 1;
+         public int PageSize { get; set; } = 2;
     }
 }
