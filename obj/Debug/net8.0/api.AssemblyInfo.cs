92c3644893be5e850e145ed7c6ac2713b1b3285e
pawan kumar <pawan59c@gmail.com>
2024-09-21 10:40:57 +1000

    added appuser package

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4ed36d4a38c76b61ee4a3ee1e404760eda3a708")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a14b5587a3a9d6f3241e83876b212b04dc369f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
