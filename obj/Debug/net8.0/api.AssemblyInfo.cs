68b3feb5f566ebf8ee315a7b379a795114dc2d3c
pawan kumar <pawan59c@gmail.com>
2024-09-28 11:12:13 +1000

    added filter

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92c3644893be5e850e145ed7c6ac2713b1b3285e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43c6c4943752bcde8fa19acb810b42cb5a681956")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
