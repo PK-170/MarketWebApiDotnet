fca2dfe47373ca2f77eeb5e8f8e7f064740b4e6f
pawan kumar <pawan59c@gmail.com>
2024-10-01 21:03:59 +1000

    added JWT Identity Account Controller

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers
@@ -11,6 +13,12 @@ namespace api.Controllers
 
     public class AccountController: ControllerBase
     {
+        private readonly UserManager<AppUser> _userManager;
+        public AccountController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         
     }
 }
