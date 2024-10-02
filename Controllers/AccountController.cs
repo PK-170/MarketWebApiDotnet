90f3cde632396fb4ff669e3cf688cb7ae10acb55
pawan kumar <pawan59c@gmail.com>
2024-10-02 21:11:13 +1000

    added register Dto

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Dtos.Account;
 using api.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
