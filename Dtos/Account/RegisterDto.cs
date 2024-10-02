d3386b37c880d3b21fca389f4978f776df927090
pawan kumar <pawan59c@gmail.com>
2024-10-02 21:13:07 +1000

    added register Dto

--- Dtos/Account/RegisterDto.cs
+++ Dtos/Account/RegisterDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace api.Dtos.Account
 {
     public class RegisterDto
     {
-        
+        [Required]
+        public string? UserName { get; set; }
     }
 }
