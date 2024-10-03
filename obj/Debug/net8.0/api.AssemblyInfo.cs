df3674517b0892d6d803efbcf889eb90954f2083
pawan kumar <pawan59c@gmail.com>
2024-10-03 18:11:50 +1000

    Token Service interface

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38034e48e023bd010b4fcb5ce3a2deb9b22019ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c4f9ee982ecb912797b35529d983842ccbd97e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
