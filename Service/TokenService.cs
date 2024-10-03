e42d2a5726079ff09ed5aeea948497ae6f2a62c0
pawan kumar <pawan59c@gmail.com>
2024-10-03 18:18:06 +1000

    Token Service class

--- Service/TokenService.cs
+++ Service/TokenService.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using api.Interfaces;
 using api.Models;
+using Microsoft.IdentityModel.Tokens;
 
 namespace api.Service
 {
     public class TokenService : ITokenService
     {
+        private readonly IConfiguration _config;
+        private readonly SymmetricSecurityKey _key;
         public string CreateToken(AppUser user)
         {
             throw new NotImplementedException();
